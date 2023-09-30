f736b6fdb55a41cf75b5d7fba19e542824fb378b
fernanzp <fperez23@ucol.mx>
2023-09-29 21:36:32 -0600

    Create the project, add the screen and the forest backgound

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame, sys
+from pygame.locals import *
+
+#Iniciación de pygame
+pygame.init()
+
+#Creamos la pantalla
+weight, hight = 1280,720
+screen = pygame.display.set_mode((weight,hight))
+pygame.display.set_caption('EarthCare')
+
+#Background
+background = pygame.image.load("Backgrounds/Forest/Background_forest_0.png").convert()
+x=0
+
+#Bucle game
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    x_relativa = x % background.get_rect().width
+    screen.blit(background, (x_relativa - background.get_rect().width, 0))
+    if x_relativa < weight:
+        screen.blit(background,(x_relativa,0))
+
+    x -= 1
+    pygame.display.update()
