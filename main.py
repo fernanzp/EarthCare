a0c5744d77d3e75ac6609fd0140cd4a6ad8ab950
fernanzp <fperez23@ucol.mx>
2023-10-01 19:15:43 -0600

    add the male caracter and give it movement

--- main.py
+++ main.py
@@ -5,24 +5,89 @@ from pygame.locals import *
 pygame.init()
 
 #Creamos la pantalla
-weight, hight = 1280,720
+weight, hight = 1280,720 #Variables para definir el ancho y el alto de la pantalla
 screen = pygame.display.set_mode((weight,hight))
-pygame.display.set_caption('EarthCare')
+pygame.display.set_caption('EarthCare') #Nombre del juego
+run = True
+
+#set framerate
+clock = pygame.time.Clock()
+fps = 60
 
 #Background
-background = pygame.image.load("Backgrounds/Forest/Background_forest.png").convert()
+background = pygame.image.load('Resourses/Backgrounds/Forest/Background_forest.png').convert() #Creamos el fondo
 x=0
 
+'''#Personajes
+quieto = pygame.image.load('Resourses/Characters/Personaje_H.png')'''
+
+#Datos personajes
+class players(pygame.sprite.Sprite):
+    def __init__(self, x, y, velocidad):
+        pygame.sprite.Sprite.__init__(self)
+        self.velocidad = velocidad
+        quieto = pygame.image.load('Resourses/Characters/Personaje_H.png')
+        self.scale = pygame.transform.scale(quieto, (int(quieto.get_width()), int(quieto.get_height())))
+        self.rect = quieto.get_rect()
+        self.rect.center = (x, y)
+
+    def move(self, izquierda, derecha):
+        #Reset movement variables
+        dx = 0
+        dy = 0
+
+        #Assign movement variables if moving left or right
+        if izquierda:
+            dx = -self.velocidad
+        if derecha:
+            dx = self.velocidad
+
+        #Update rectangle position
+        self.rect.x += dx
+        self.rect.y += dy
+    def draw(self):
+        screen.blit(self.scale, self.rect)
+
+player_h = players(200,650,4)
+
+#Definimos las variables de accion de los player
+izquierda = False
+derecha = False
+
 #Bucle game
-while True:
+while run:
+    clock.tick(fps)
     for event in pygame.event.get():
+        #Quit game
         if event.type == QUIT:
+            run = False
             pygame.quit()
             sys.exit()
+
+        #Keyboard presses
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                izquierda =True
+            if event.key == pygame.K_d:
+                derecha = True
+            if event.key == pygame.K_ESCAPE:
+                run = False
+
+        #Keyboard button release
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                izquierda = False
+            if event.key == pygame.K_d:
+                derecha = False
+
+    #Fondo movimiento
     x_relativa = x % background.get_rect().width
     screen.blit(background, (x_relativa - background.get_rect().width, 0))
     if x_relativa < weight:
         screen.blit(background,(x_relativa,0))
-
     x -= 1
+
+    player_h.draw()
+    player_h.move(izquierda, derecha)
+
     pygame.display.update()
