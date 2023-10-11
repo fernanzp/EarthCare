88b717b26395b1ca8cc7b2ec920c036f6256e57b
fernanzp <fperez23@ucol.mx>
2023-10-11 14:33:25 -0600

    Add Trash

--- trash.py
+++ trash.py
@@ -4,15 +4,15 @@ import pygame
 class Trash(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load('Resourses/Trash/Bannana.png').convert()
-        self.image = pygame.transform.scale(self.image, (40, 40))
+        self.image = pygame.image.load('Resourses/Trash/Apple_pix.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (90, 90))
         self.rect = self.image.get_rect()
         self.reset()
 
     def reset(self):
         self.rect.x = random.randrange(1280 - self.rect.width)
         self.rect.y = -self.rect.width
-        self.velocity_y = 5
+        self.velocity_y = random.randrange(2, 4)
 
     def update(self):
         self.rect.y += self.velocity_y
