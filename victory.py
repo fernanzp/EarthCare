d268341f47391fa3848f1117fecd039fda134a6a
fernanzp <fperez23@ucol.mx>
2023-11-17 23:44:09 -0600

    Score background

--- victory.py
+++ victory.py
@@ -20,7 +20,7 @@ class VictoryScreen:
         self.victory_title = pygame.image.load('Resourses/Titles/Victory_title.png').convert_alpha()
 
         #Score victory
-        self.score_background = pygame.image.load('Resourses/Backgrounds/Score/Score_background.png').convert_alpha()
+        self.score_background = pygame.image.load('Resourses/Backgrounds/Score/Score_background_4a.png').convert_alpha()
 
         #Buttons
         #self.menu_button = pygame.image.load('Resourses/Buttons/Menu_button.png').convert_alpha()
@@ -71,8 +71,8 @@ class VictoryScreen:
             text_metal = self.get_font(18).render(f"Metal:{self.metal}", True, (255, 0, 0))
             text_paper = self.get_font(18).render(f"Paper:{self.paper}", True, (255, 255, 0))
 
-            # Score background
-            #self.display_surface.blit(self.score_background, (x_counter, 370))
+            #Score background
+            self.display_surface.blit(self.score_background, (x_counter-30, 378))
 
             self.display_surface.blit(text_organic, (x_counter, 420)) #50 50
             self.display_surface.blit(text_glass, (x_counter, 470))
